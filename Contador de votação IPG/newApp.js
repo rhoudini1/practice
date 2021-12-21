ac18de302b28ae7d1b06c4e12d79b5bc5fbe372e
Renan <araujomonteirogba@hotmail.com>
2021-12-21 15:41:11 -0300

    Versão 1.0 definitiva. Funcionando com alguns bugs, mas estável de modo geral

--- Contador de votação IPG/newApp.js
+++ Contador de votação IPG/newApp.js
@@ -4,6 +4,7 @@
 
 let presbs = []
 let diacs = []
+let totalVotos = 0
 
 /* ============
    ELEMENTOS
@@ -13,6 +14,8 @@ const candidateName = document.getElementById("candidate-name")
 const category = document.getElementById("category")
 const presbsField = document.getElementById("presbs-field")
 const diacsField = document.getElementById("diacs-field")
+const presbsResult = document.getElementById("presbs-results")
+const diacsResult = document.getElementById("diacs-results")
 
 /* ==========
    CLASSES
@@ -23,6 +26,7 @@ class Candidate {
         this.name = name
         this.votes = 0
         this.category = ""
+        this.elect = ""
     }
 }
 
@@ -52,7 +56,7 @@ function renderPresbs() {
     let presbsRendered = `<legend class="legend">Presbíteros</legend>`
     for (let i=0; i < presbs.length; i++) {
         presbsRendered += `<div>
-            <input type="checkbox" name="candidato" id="candidatoA">
+            <input type="checkbox" name="candidato" id="checkPresb${i}">
             <label for="${presbs[i]}">${presbs[i].name}</label>
             <button type="button" onclick="deletePresb(${i})">Excluir</button>
         </div>`
@@ -60,6 +64,15 @@ function renderPresbs() {
     presbsField.innerHTML = presbsRendered
 
     // ADICIONAR RENDER DO RESULTADO
+    let presbsRenderedResult = `<legend class="legend">Presbíteros</legend>`
+    if (totalVotos > 0) {
+        for (let i=0; i < presbs.length; i++) {
+            presbsRenderedResult += `<div>
+            ${presbs[i].name}: ${presbs[i].elect} com ${(presbs[i].votes / totalVotos * 100).toFixed(2)}% dos votos.
+            </div>`
+    }
+    }
+    presbsResult.innerHTML = presbsRenderedResult
 }
 
 
@@ -67,7 +80,7 @@ function renderDiacs() {
     let diacsRendered = `<legend class="legend">Diáconos</legend>`
     for (let i=0; i < diacs.length; i++) {
         diacsRendered += `<div>
-            <input type="checkbox" name="candidato" id="candidatoA">
+            <input type="checkbox" name="candidato" id="checkDiac${i}">
             <label for="${diacs[i]}">${diacs[i].name}</label>
             <button type="button" onclick="deleteDiac(${i})">Excluir</button>
         </div>`
@@ -75,6 +88,15 @@ function renderDiacs() {
     diacsField.innerHTML = diacsRendered
 
     // ADICIONAR RENDER DO RESULTADO
+    let diacsRenderedResult = `<legend class="legend">Diáconos</legend>`
+    if (totalVotos > 0) {
+        for (let i=0; i < diacs.length; i++) {
+            diacsRenderedResult += `<div>
+            ${diacs[i].name}: ${diacs[i].elect} com ${(diacs[i].votes / totalVotos * 100).toFixed(2)}% dos votos.
+            </div>`
+    }
+    }
+    diacsResult.innerHTML = diacsRenderedResult
 }
 
 
@@ -91,9 +113,27 @@ function deleteDiac(id) {
 
 
 function saveVote() {
-    //captar botões
+    totalVotos++
+    
+    //captar botões presbiteros com fulano.votes++
+    for (let i=0; i < presbs.length; i++) {
+        document.getElementById(`checkPresb${i}`).checked && presbs[i].votes++
+        presbs[i].votes / totalVotos >= 0.5
+            ? presbs[i].elect = "Eleito"
+            : presbs[i].elect = "Não eleito"
+    }
 
-    // fulano.votes++
+    // captar botoes diaconos
+    for (let i=0; i < diacs.length; i++) {
+        document.getElementById(`checkDiac${i}`).checked && diacs[i].votes++
+        diacs[i].votes / totalVotos >= 0.5
+            ? diacs[i].elect = "Eleito"
+            : diacs[i].elect = "Não eleito"
+    }
 
-    // checkboxes.value = null
+    // renderizar total de votos
+    const resultadoFinal = document.getElementById('votos-el')
+    resultadoFinal.textContent = `${totalVotos}`
+    renderPresbs()
+    renderDiacs()
 }
